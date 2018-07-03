2b8dbb0ad4339a8c72082fb9da29207a2f0969d2
Enrique Calvo <encalram@gmail.com>
2018-07-03 13:32:01 +0200

    inital

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import StyleList from './stylelist/stylelist';
+import StyleList form './stylelist/stylelist.js';
+
+ReactDOM.render(
+    <StyleList />,
+  document.getElementById('app')
+);
---
+++ src/stylelist/stylelist.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './style.css';
+
+export default class StyleList extends React.Component {
+    
+}
