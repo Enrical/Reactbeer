2a0f18b7f73410e8981797b6c3fc449b33be8aeb
Enrique Calvo <encalram@gmail.com>
2018-07-03 14:21:28 +0200

    asdf

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
-import StyleList from './stylelist/stylelist';
-import StyleList form './stylelist/stylelist.js';
+import StyleList from './stylelist/stylelist.js';
 
 ReactDOM.render(
     <StyleList />,
