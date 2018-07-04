9743b276e8efd844fd6c774dc3cc3794e428d649
Enrique Calvo <encalram@gmail.com>
2018-07-04 15:10:16 +0200

    header

--- my-app/src/Header/header.js
+++ my-app/src/Header/header.js
@@ -13,14 +13,14 @@ export default class Header extends React.Component {
                             <div class="bg p-4">
                                 <ul class="nav navbar-nav">
                                     <li class="active">
-                                        <a href="#">Home</a>
+                                        <a href="">Home</a>
                                     </li>
                                     <li class="dropdown">
-                                        <a href="#"> browse beers
+                                        <a href=""> browse beers
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="#">contact</a>
+                                        <a href="">contact</a>
                                     </li>
                                 </ul>
                             </div>
@@ -37,10 +37,10 @@ export default class Header extends React.Component {
                         
                         <ul class="nav navbar-nav navbar-right"  id="singup">
                             <li>
-                                <a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
+                                <a href=""><span class="glyphicon glyphicon-user"></span> Sign Up</a>
                             </li>
                             <li>
-                                <a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+                                <a href=""><span class="glyphicon glyphicon-log-in"></span> Login</a>
                             </li>
                         </ul>
 
