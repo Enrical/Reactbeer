84bdec6dd17754eaffd28cd92fc12df626e97e4e
Enrique Calvo <encalram@gmail.com>
2018-07-04 09:50:30 +0200

    I am going to merge to the master

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default class App extends React.Component {
     render() {
         return (
         <div>
-            <Header ok="ok" />
+            <Header />
             <Page />
         </div>
         )
