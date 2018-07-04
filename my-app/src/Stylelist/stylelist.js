6f84c0f002090738f03f088be922ab8569f94eea
Enrique Calvo <encalram@gmail.com>
2018-07-04 14:42:04 +0200

    my app

---
+++ my-app/src/Stylelist/stylelist.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './stylelist.css';
+import BeerStyle from '../BeerStyle/beerStyle.js';
+
+
+
+export default class StyleList extends React.Component {
+    constructor (props) {
+        super (props);
+        this.state = {
+            styles : [
+                {
+                    name: 'Style 1',
+                    description : 'description1'
+                },
+                {
+                    name: 'Style 2',
+                    description : 'description 2'
+                },
+                {
+                    name: 'Style 3',
+                    description : 'description 3'
+                },
+                {
+                    name: 'Style 4',
+                    description : 'description 4'
+                },
+                {
+                    name: 'Style 5',
+                    description : 'description 5'
+                }
+
+            ]
+        }
+    }
+
+    render() {
+        const {styles} = this.state;
+        console.log(styles);
+
+        return (
+            <div className="box">
+                {
+                    styles.map((style, i) => {
+                        return (
+                            <BeerStyle key={i} name={style.name} description={style.description} />
+                        )
+                    })
+                }
+            </div>
+        );
+    }
+}
