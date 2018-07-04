525e4a81f6bcacd1a525afdd22eb903edcd1f5c0
Sean Hochman <seanhochman@Seans-MacBook-Air.local>
2018-07-04 14:13:56 +0200

    contact us

--- src/stylelist/stylelist.js
+++ src/stylelist/stylelist.js
@@ -40,6 +40,7 @@ export default class StyleList extends React.Component {
 
         return (
             <div className="box">
+            <div className="list">
                 {
                     styles.map((style, i) => {
                         return (
@@ -47,6 +48,7 @@ export default class StyleList extends React.Component {
                         )
                     })
                 }
+            </div>
             </div>
         );
     }
