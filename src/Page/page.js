382229359cf5687a3c627f9d49e6fcb62522d3a7
Enrique Calvo <encalram@gmail.com>
2018-07-04 10:46:32 +0200

    user login

--- src/Page/page.js
+++ src/Page/page.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './page.css';
+import Userlogin from '../Userlogin/userlogin';
 
 export default class Page extends React.Component {
     render() {
         return (
             
             <div className="box">
+            <Userlogin />
             </div>
         )
     }
